70da5ceed1b343bdfea27308ca54badde3a6224d
sajay <zajubabu7@gmail.com>
2026-04-27 23:01:38 +0530

    merged base setup from weekone and configure app.js for signup

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
+const path = require('path');
+const userRoutes = require('./routes/userRoutes'); // Import your routes
 
 dotenv.config();
 const app = express();
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 connectDB();
 
-app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
+app.use('/', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));;
